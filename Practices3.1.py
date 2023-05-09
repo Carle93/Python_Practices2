baac8fbc82117da564fb2f12a5389dace9d0495d
carlexi niño <isley7777@gmail.com>
2023-05-09 17:49:20 -0500

    Mablab

--- Practices3.1.py
+++ Practices3.1.py
@@ -6,3 +6,11 @@ def fibonacci (n):
     
 print (fibonacci (2))
 
+# Juego : MadLab F-string 
+Adj = input("adjetivo:  ")
+Verbo1 = input("Verbo:  ")
+Verbo2 = input("Verbo:  ")
+Sustantivo_plural = input ("sustantivo (plural):  ")
+
+Mad_lab = f" Si me siento {Adj} , puede que tenga que {Verbo1}, para lograr completar la actividades que tengo que {Verbo2} para no procratisnar."
+print (Mad_lab)
